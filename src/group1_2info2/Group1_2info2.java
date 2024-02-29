8493e700346c764e20bd3201e672be4bb244d929
Mohamedgcv <mahjoubmohamed.gcv@gmail.com>
2024-02-29 19:06:33 +0100

    functions added

--- src/group1_2info2/Group1_2info2.java
+++ src/group1_2info2/Group1_2info2.java
@@ -14,8 +14,37 @@ public class Group1_2info2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+
+public static int puissance (int a, int b ){
+    int p=1;
     
+    if (b>=1){
+    for (int i=1; i<=b; i++) {
+        p = p*a;
+    }
+}
+return p;
+}
+public static int paire (int a){
+    if ((a/2)==0) return 1;
+}
+
+
+
+public static void main(String[] args) {
+     
+
+    system.out.println(paire(2));
+}
+
+
+
+
+
+
+
+
+
+
 }
